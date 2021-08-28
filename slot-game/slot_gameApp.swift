ff4a85d71ab37000054273ac2cba38dafdcb92f6
ganudoomer <ganuiscool@gmail.com>
2021-08-28 12:11:44 +0530

    Initial Commit

---
+++ slot-game/slot_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  slot_gameApp.swift
+//  slot-game
+//
+//  Created by Sree on 28/08/21.
+//
+
+import SwiftUI
+
+@main
+struct slot_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
